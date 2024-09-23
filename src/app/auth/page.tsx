66bdad6f0b967714b9079d804295b29615db6fb6
mejzerVitaliy <mejzervitalik@gmail.com>
2024-09-23 11:49:15 +0300

    set routes

---
+++ src/app/auth/page.tsx
@@ -0,0 +1,12 @@
+// Страница, где будет возможность продолжить как гость или войти как администратор
+import React from 'react'
+
+const page:React.FC = () => {
+    return (
+        <main>
+            Auth page
+        </main>
+    )
+}
+
+export default page
---
+++ src/app/to_admin_account/page.tsx
@@ -0,0 +1,12 @@
+//Страница с формой для входа в админку
+import React from 'react'
+
+const page:React.FC = () => {
+    return (
+        <main>
+            To admin form 
+        </main>
+    )
+}
+
+export default page
